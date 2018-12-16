86bfc47d22af6716cfcf81c2b54d1c31ecc0d5f9
JussiHuot <jussi.huotari@aalto.fi>
2018-12-16 17:37:15 +0200

    fix fix

--- frontend/src/api/getTrashbinData.js
+++ frontend/src/api/getTrashbinData.js
@@ -1,27 +1,6 @@
 const GetTrashbinData = async () => {
 
-    // return [
-    //     {
-    //         id: 1,
-    //         owner: "Aalto",
-    //         address: "Otakaari 5",
-    //         bintype: "Large Bio Waste",
-    //         latestEvent: "Opened yesterday",
-    //         status: "OK"
-    //     },
-    //     {
-    //         id: 2,
-    //         owner: "Aalto",
-    //         address: "Otakaari 7",
-    //         bintype: "Small Bio Waste",
-    //         latestEvent: "Opened today",
-    //         status: "FULL"
-    //     },
-    // ]
-
-    const url = 'http://localhost:3001/trashbins';
-    
-    const response = await fetch(url);
+    const response = await fetch('http://localhost:3001/trashbins');
     const body = await response.json();
     return body.express;
   }
