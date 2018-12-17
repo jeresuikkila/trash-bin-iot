211cea999b89c0556668ccad2c34a47df67351ad
Veera Alt <veera.alt@aalto.fi>
2018-12-18 00:00:45 +0200

    Create trashbin details view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import TrashBinRow from './components/TrashBinRow';
-
-// eslint-disable-next-line
-import GetTrashbinData from './api/getTrashbinData';
-
+import MainPage from './components/MainPage';
+import TrashBinDetails from './components/TrashBinDetails'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { trashbins: [] }
+  constructor(props) {
+    super(props)
+    this.state = { selectedBin: null }
   }
-  componentWillMount() {
-    GetTrashbinData().then(bins => {
-      this.setState({trashbins: bins});
-    });
-  }
-  render() {
-    let trashbins = this.state.trashbins
-    console.log("trashbins: ", trashbins)
-    console.log("test: ", trashbins[0])
-    return (
-      <div className="container">
-        <h1>Trash Bin IoT</h1>
-        <h2>Trash bins</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Bin ID</th>
-              <th scope="col">Property manager</th>
-              <th scope="col">Address</th>
-              <th scope="col">Type</th>
-              <th scope="col">Latest event</th>
-              <th scope="col">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {trashbins.map(trashbin => 
-              <TrashBinRow key={trashbin.id}
-                bin={trashbin} />
-            )}
-          </tbody>
-        </table>
-      </div>
 
-    );
+  setBinSelected = bin =>
+    this.setState({ selectedBin: bin })
+
+  render() {
+    return this.state.selectedBin === null ? 
+      <MainPage setBinSelected={this.setBinSelected} /> :
+      <TrashBinDetails
+        trashbin={this.state.selectedBin}
+        setBinSelected={this.setBinSelected}/>
+      
   }
 }
 
--- frontend/src/components/TrashBinRow.js
+++ frontend/src/components/TrashBinRow.js
@@ -3,7 +3,13 @@ import React from 'react';
 const TrashBinRow = (props) => {
     return (
       <tr>
-        <td><a href={`/trashbin/${props.bin.id}`}>{props.bin.id}</a></td>
+        <td>
+          <button className="btn btn-link" onClick={
+            () => props.setBinSelected(props.bin)
+          }>
+            {props.bin.id}
+          </button>
+        </td>
         <td>{props.bin.owner}</td>
         <td>{props.bin.address}</td>
         <td>{props.bin.bintype}</td>
