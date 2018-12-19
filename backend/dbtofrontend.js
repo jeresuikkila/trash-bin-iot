8d9b4d1362a6ee01e2d16580179d019a3a5803cf
vchau <van.chau@aalto.fi>
2018-12-19 10:50:30 +0200

    fix dbtofrontend.js

--- backend/dbtofrontend.js
+++ backend/dbtofrontend.js
@@ -1,32 +1,39 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const cors = require('cors');
+const port = 3001;
+const models = require('./models');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Listening on port ${port}`));
+const db = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    });
 
 
-exports.sendData = function (models, app) {
     models.trashbin.findAll({
-        attributes: ['id', 'bintype', 'owner', 'address']
+        attributes: ['id','bintype','owner','address']
     }).then(bins => {
-        var arr = [];
-        bins.forEach(bin => {
+        var arr=[];
+        bins.forEach(bin =>{
             bin.dataValues.latestEvent = "test event",
-                bin.dataValues.status = "test status"
+            bin.dataValues.status = "test status"
             bin.dataValues.id = bin.dataValues.id.toString();
             arr.push(bin.dataValues);
         });
         console.log(arr);
         app.get('/trashbins', (req, res) => {
-            res.send({ express: arr });
-        });
+            res.send({ express: arr});
+          });
     });
 
-    models.event.findAll({
-        attributes: ['packet_hash', 'event-time', 'temperature', 'pitch', 'roll', 'touchtagDevEui']
-    }).then(events => {
-        var arr = [];
-        events.forEach(event => {
-            arr.push(bin.dataValues);
-        });
-        console.log(arr);
-        app.get('/trashbins', (req, res) => {
-            res.send({ express: arr });
-        });
-    });
-}
