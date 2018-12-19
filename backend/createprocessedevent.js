df640c91f828fb66ce186f39fe969a2a4b003a32
PatBom <45004649+PatBom@users.noreply.github.com>
2018-12-19 13:29:28 +0200

    logic extension

---
+++ backend/createprocessedevent.js
@@ -0,0 +1,37 @@
+exports.createProcessedEvent = function (message, models) {
+	const lastEventTime = models.processedevent.max('event_time').then( max => {
+		});
+	const cooldown = 60;
+		
+	// trigger code 4 stands for movement stop
+	if (Number(message.decoded_payload.trigger_code) == 4 &&
+		//compare the time when the current event happened to latest saved event. if not enough time has passed, we will not process the event
+		moment.unix(message.meta.time) >= lastEventTime - cooldown
+	){   		
+		models.sensorbin.findOne({
+			attributes: ['trashbinId'],
+			where: {
+				touchtagDevEui: message.meta.device
+			}
+		}).then(sensorbin => {
+			console.log(sensorbin.dataValues.trashbinId);
+			const trashbinid = sensorbin.dataValues.trashbinId;
+
+			models.processedevent.create({
+				packet_hash: message.meta.packet_hash,
+				event_type: "Bin opened",
+				even_time: moment.unix(message.meta.time),
+				trashbinId: trashbinid
+			}).then(() => {
+				models.processedevent.findOne({
+					where:{
+						packet_hash: message.meta.packet_hash
+					},
+					include:[
+						{ model: models.trashbin, attributes:['trashbinid'] },
+					],
+				})
+			})
+		});
+	}
+}
