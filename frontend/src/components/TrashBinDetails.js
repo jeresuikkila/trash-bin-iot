7a59cb979b2a1d7f762be244d17fa2677e717805
JussiHuot <jussi.huotari@aalto.fi>
2018-12-21 09:47:43 +0200

    last minute fix

--- frontend/src/components/TrashBinDetails.js
+++ frontend/src/components/TrashBinDetails.js
@@ -12,14 +12,37 @@ class TrashBinDetails extends React.Component {
         events: {}
     }
 
-    componentDidMount(){
-        getEventsByTrashbin(this.props.trashbin.id).then(event => this.setState({events: event, loading: false})) 
-    }    
-
+    componentDidMount() {
+        getEventsByTrashbin(this.props.trashbin.id).then(event => this.setState({ events: event, loading: false }))
+    }
+    renderSwitch(param) {
+        switch (param) {
+            case '2':
+                return 'singleclick';
+            case '3':
+                return 'movement start';
+            case '4':
+                return 'movement stop';
+            case '5':
+                return 'freefall';
+            case '8':
+                return 'doubleclick';
+            case '0':
+                return 'restart';
+            case '9':
+                return 'long click';
+            case '11':
+                return 'temp max/min';
+            case '6':
+                return 'activation';
+            default:
+                return param;
+        }
+    }
     render() {
-        if(this.state.loading){
-            return(<p>Loading...</p>)
-        } 
+        if (this.state.loading) {
+            return (<p>Loading...</p>)
+        }
         else {
             return (
                 <div>
@@ -28,30 +51,30 @@ class TrashBinDetails extends React.Component {
                             <Link to="/" onClick={() => this.props.setBinSelected(null)}>
                                 <button className="btn btn-light">
                                     Back
-                                </button>    
-                            </Link> 
+                                </button>
+                            </Link>
                             <Route exact path="/"></Route>
                         </div>
                     </Router>
-                    
-                    
+
+
                     <h3>Trash bin details</h3>
                     <p>ID: {this.props.trashbin.id}</p>
                     <p>Address: {this.props.trashbin.address}</p>
                     <p>Type: {this.props.trashbin.bintype}</p>
-                    
+
                     <table className="table">
                         <EventMenu />
                         <tr>
                             <th scope="col">Time</th>
-                            <th scope="col">Trigger Code</th>
+                            <th scope="col">Event</th>
                         </tr>
                         <tbody>
                             {this.state.events.map(event =>
-                             <tr>
-                                 <td>{event.event_time}</td>
-                                 <td>{event.trigger_code}</td>
-                             </tr>   
+                                <tr>
+                                    <td>{event.event_time}</td>
+                                    <td> {this.renderSwitch(event.trigger_code)}</td>
+                                </tr>
                             )}
                         </tbody>
                     </table>
