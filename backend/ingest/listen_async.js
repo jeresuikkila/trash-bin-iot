831e0cfba5089cd2ab89e598dfa5998ea466ee96
JussiHuot <jussi.huotari@aalto.fi>
2018-12-27 13:02:36 +0200

    rewrote listener using async/await

---
+++ backend/ingest/listen_async.js
@@ -0,0 +1,58 @@
+const axios = require('axios')  // performs http requests
+const moment = require('moment')
+
+const decoderUrl = process.env.DECODER_URL
+
+console.log(decoderUrl)
+
+exports.listenTouchtags = (models, app, processedevent) => {
+    app.post('*', async (req, res) => {
+        try {
+            const message = req.body; // one event message from sensor
+            console.log(message.params.payload);
+            // Send payload to decoder
+            const response = await axios.post(decoderUrl, {
+                "payload": message.params.payload
+            });
+            message['decoded_payload'] = JSON.parse(response.data.body)
+            //creates new event in database or finds one if it already exists
+            const resp = await models.event.findOrCreate({
+                where: {
+                    packet_hash: message.meta.packet_hash
+                },
+                defaults: {
+                    packet_hash: message.meta.packet_hash,
+                    payload: message.params.payload,
+                    original_message: message,
+                    event_time: moment.unix(message.meta.time),
+                    temperature: message.decoded_payload.temperature,
+                    trigger_code: message.decoded_payload.trigger_code,
+                    trigger_counter: message.decoded_payload.trigger_counter,
+                    pitch: message.decoded_payload.pitch,
+                    roll: message.decoded_payload.roll,
+                    touchtagDevEui: message.meta.device
+                }
+            });
+            console.log('Is new event? ', resp[0]._options.isNewRecord);
+            //adds foreign key if new event
+            if (resp[0]._options.isNewRecord) {
+                await models.event.findOne({
+                    where: {
+                        packet_hash: message.meta.packet_hash
+                    },
+                    include: [
+                        { model: models.touchtag, attributes: ['dev_eui'] },
+                    ],
+                });
+                res.status(200).send("added to database.");
+                processedevent.createProcessedEvent(message,models,moment);
+            }
+            else {
+                res.status(500).send("already in database.");
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send(e);
+        }
+    });
+}
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ require('dotenv').config({ path: '../.env' });
 const port = process.env.APIPORT | 3001;
 const listenerport = process.env.LISTENERPORT | 3002;
 const models = require('./models');
-const listener = require('./ingest/listen_seq')
+//const listener = require('./ingest/listen_seq')
+const listener = require('./ingest/listen_async')
 const processedevent = require('./createprocessedevent')
 
 
