9087869ed74acf9d5d351d53482ddb08e7b343b0
JussiHuot <jussi.huotari@aalto.fi>
2018-12-27 22:24:36 +0200

    fix

--- backend/listenerapp.js
+++ backend/listenerapp.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
     //intercepts OPTIONS method
     if ('OPTIONS' === req.method) {
       //respond with 200
-      res.send(200);
+      res.sendStatus(200);
     }
     else {
     //move on
@@ -23,7 +23,7 @@ app.options("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
+    res.sendStatus(200);
   });
 
 module.exports = app;
