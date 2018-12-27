9642999a4fae51f55e93a9f26155bcd1de45403c
JussiHuot <jussi.huotari@aalto.fi>
2018-12-27 22:41:19 +0200

    fix

--- backend/listenerapp.js
+++ backend/listenerapp.js
@@ -7,9 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.options("*", function(req, res, next){
     console.log("options?")
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.sendStatus(200);
   });
 
