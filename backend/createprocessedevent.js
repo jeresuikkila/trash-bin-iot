85c24e949b6399c80ab07d8748735300dd5dd980
JussiHuot <jussi.huotari@aalto.fi>
2019-01-03 16:53:50 +0200

    fixed stuff on processedevents

--- backend/createprocessedevent.js
+++ backend/createprocessedevent.js
@@ -1,55 +1,54 @@
-exports.createProcessedEvent = function (message, models,moment) {
+exports.createProcessedEvent = async (message, models, moment) => {
 	console.log("creaeprocessedevent started");
 
-	var lastEventTime = Number( models.processedevent.max('event_time').then( max => {}) );
-	lastEventTime = lastEventTime || 0; //returns 0 if lastEventTime == falsey
-	const cooldown = 60;		
+	const cooldown = 60;
 	// trigger code 4 stands for movement stop
-	if (Number(message.decoded_payload.trigger_code) == 4/* &&
-		//compare the time when the current event happened to latest saved event. if not enough time has passed, we will not process the event
-		moment.unix(message.meta.time) - cooldown >= lastEventTime*/ 
-		){  
-		console.log("event triggercode = 4");
-		var time=moment.unix(message.meta.time).format();
-		var time2=moment.unix(message.meta.time-cooldown).format();
-		console.log("time between ",time2,"  and  ",time)
-		models.sensorbin.findOne({
-			attributes: ['trashbinId'],
-			where: {
-				touchtagDevEui: message.meta.device
-			}
-		}).then(sensorbin => {
-			console.log("sensorbin: ",sensorbin.dataValues);
-
-			models.processedevent.findOrCreate({
+	if (Number(message.decoded_payload.trigger_code) == 4) {
+		try {
+			console.log("event triggercode = 4");
+			var time = moment.unix(message.meta.time + cooldown).format();
+			var time2 = moment.unix(message.meta.time - cooldown).format();
+			console.log("time between ", time2, "  and  ", time)
+			const sensorbin = await models.sensorbin.findOne({
+				attributes: ['trashbinId'],
 				where: {
-					//packet_hash: message.meta.packet_hash
-					 trashbinId: sensorbin.dataValues.trashbinId,
-					 event_time: {
-					 	"$between": [time2,time]
-					 }
+					touchtagDevEui: message.meta.device
+				}
+			});
+			console.log("sensorbin: ", sensorbin.dataValues);
+
+			const event = await models.processedevent.findOrCreate({
+				where: {
+					trashbinId: sensorbin.dataValues.trashbinId,
+					event_time: {
+						"$between": [time2, time]  //if another event between eventtime+cooldown and eventtime-cooldown finds it instead creating new one
+					}
 				},
 				defaults: {
-				packet_hash: message.meta.packet_hash,
-				event_type: "Bin opened",
-				event_time: moment.unix(message.meta.time),
-				trashbinId: sensorbin.dataValues.trashbinId
+					packet_hash: message.meta.packet_hash,
+					event_type: "Bin opened",
+					event_time: moment.unix(message.meta.time),
+					trashbinId: sensorbin.dataValues.trashbinId
 				}
-			}).then(() => {
+			});
+			if (event[0]._options.isNewRecord) {
 				models.processedevent.findOne({
-					where:{
+					where: {
 						packet_hash: message.meta.packet_hash
 					},
-					include:[
-						{ model: models.trashbin, attributes:['id'] },
+					include: [
+						{ model: models.trashbin, attributes: ['id'] },
 					],
-				})
-			}).catch(err => {
-				console.log("lul",err)
-			})
-		});
+				});
+			}
+			else {
+				console.log("didn't create event because cooldown not done.")
+			}
+		} catch (e) {
+			console.log("error creating processed event: ", e)
+		}
 	}
-	else{
+	else {
 		console.log("nothing interesting happens")
 	}
 }
