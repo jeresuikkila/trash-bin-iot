9c3125d5a0c5a51213befd7cdd4502ff2aaf5774
JussiHuot <jussi.huotari@aalto.fi>
2019-01-21 15:39:39 +0200

    test

--- backend/ingest/createprocessedevent.js
+++ backend/ingest/createprocessedevent.js
@@ -18,7 +18,9 @@ exports.createProcessedEvent = async (message, models, moment) => {
 					touchtagDevEui: message.meta.device
 				}
 			});
-			console.log("sensorbin[0]: ", sensorbin[0])
+			console.log("sensorbin: ",sensorbin);
+			console.log("sensorbin[0]: ", sensorbin[0]);
+			console.log("sensorbin.dataValues: ",sensorbin.dataValues);
 			console.log("TAGLOCATION: ",sensorbin.dataValues.taglocation);
 			if (sensorbin.dataValues.taglocation == "lid") {
 				//wait 60sec to give "priority" to bin emptied
