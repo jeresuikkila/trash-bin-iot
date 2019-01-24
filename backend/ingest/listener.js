9b5151970954aaa7a6b256abc625ec28599f474a
JussiHuot <jussi.huotari@aalto.fi>
2019-01-24 13:13:28 +0200

    test

--- backend/ingest/listener.js
+++ backend/ingest/listener.js
@@ -110,7 +110,7 @@ handleDownlink = async (message) => {
         const response = await axios.post(decoderUrl, {
             "payload": message.params.payload
         });
-        console.log("response.data.body: ",response.data.body);
+        console.log("response: ",response);
         message['decoded_payload'] = JSON.parse(response.data.body);
         console.log("DOWNLINK DPAYLOAD: ",message.decoded_payload);
     } catch (e) {
