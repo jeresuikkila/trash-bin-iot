399ce6647b9b9664ac36d77e4f1b835af7c3d625
Katariina Tuovinen <Kata@dhcp-86-252.eduroam.aalto.fi>
2019-01-24 13:34:46 +0200

    comments added

--- backend/ingest/createprocessedevent.js
+++ backend/ingest/createprocessedevent.js
@@ -1,26 +1,27 @@
 // Creates a new processed event and adds it to the database
-// Currently only trigger code 4 (movement stop event/Bin opened) is implemented
+// Currently only trigger code 3 & 4 (movement start/stop event/Bin opened) are implemented
 
+// Sleeps / waits execution for certain time
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 exports.createProcessedEvent = async (message, models, moment) => {
 	const cooldown = 30;
-	// Trigger code 4 stands for movement stop
+	// Trigger code 3 = movement start & 4 = movement stop
 	if (Number(message.decoded_payload.trigger_code) == 4 || Number(message.decoded_payload.trigger_code) == 3) {
 		try {
-			console.log("Event triggercode = 4||3");
+			console.log("Event triggercode = 3 || 4");
 			// Finds the sensorbin with deviceId
 			const sensorbin = await models.sensorbin.findOne({
-				attributes: ['trashbinId','taglocation','default_pitch','default_roll'],
+				attributes: ['trashbinId', 'taglocation', 'default_pitch', 'default_roll'],
 				where: {
 					touchtagDevEui: message.meta.device
 				}
 			});
-			console.log("sensbin.datavalues: ")
+				// If touchtag is located at the lid, the event is "bin opened"
 			if (sensorbin.dataValues.taglocation == "lid" && sensorbin.dataValues.default_pitch != null) {
-				//wait 60sec to give "priority" to bin emptied
+				// Wait 60 sec to give "priority" to bin emptied
 				console.log("WAITING 10S");
 				await sleep(10000);
 				console.log("10S DONE");
@@ -58,8 +59,9 @@ exports.createProcessedEvent = async (message, models, moment) => {
 					console.log("Didn't create event because cooldown not done.")
 				}
 			}
+			// If touchtag is located at the bottom of the bin, event is "bin emptied"
 			else if (sensorbin.dataValues.taglocation == "bottom" && sensorbin.dataValues.default_pitch != null) {
-				//cant be emptied 5mins before/after
+				// Cant be emptied 5 mins before/after
 				var time = moment.unix(message.meta.time + 300).format();
 				var time2 = moment.unix(message.meta.time - 300).format();
 				const event = await models.processedevent.findOrCreate({
