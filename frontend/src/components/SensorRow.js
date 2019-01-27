c4df27ee8d4b16fcfd226fd0514358b1603bfe6c
Veera Alt <veera.alt@aalto.fi>
2019-01-27 21:54:53 +0200

    Trashbin details view styling

--- frontend/src/components/SensorRow.js
+++ frontend/src/components/SensorRow.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import './CSS/SensorRow.css'
 
-const TrashBinRow = (props) => {
-  return (
-      <tr>
-      <td>{props.sensor.id}</td>
-      <td>{props.sensor.taglocation}</td>
-      </tr>
-  )
+class SensorRow extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      tagloc: 'Bottom sensor',
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.sensor.taglocation === 'lid') {
+      this.setState({
+        tagloc: 'Top sensor'
+      });
+    }
+  }
+
+  render() {
+    let tagloc = this.state.tagloc;
+    return (
+      <div className="sensor-row">
+        <div style={{ flex: 1 }}>
+          {tagloc}
+        </div>
+        <div style={{ flex: 3 }}>
+          id: {this.props.sensor.id}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default TrashBinRow;
+export default SensorRow;
