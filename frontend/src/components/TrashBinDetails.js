1b5515187649811d4a93a8bec9aa471cbce86b27
PatBom <45004649+PatBom@users.noreply.github.com>
2019-01-28 16:00:03 +0200

    Update TrashBinDetails.js

--- frontend/src/components/TrashBinDetails.js
+++ frontend/src/components/TrashBinDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import './TrashBinDetails.css';
 import EventMenu from './EventMenu'
 import getPEventsByTrashbin from '../api/getPEventsByTrashbin'
+import getEventsByTrashbin from '../api/getEventsByTrashbin'
 import getSingleTrashbinData from '../api/getSingleTrashbinData'
 import getSensorsByTrashbin from '../api/getSensorsByTrashbin';
 import SensorRow from './SensorRow'
@@ -13,8 +14,10 @@ class TrashBinDetails extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+			test123: 0,
             loading: true,
-            events: [],
+            pevents: [],
+			events: [],
             trashbin: {},
             sensors: []
         }
@@ -27,13 +30,39 @@ class TrashBinDetails extends React.Component {
     async componentDidMount() {
         var id = window.location.pathname.replace('/', '');
         this.setState({
-            events: await getPEventsByTrashbin(id),
+            pevents: await getPEventsByTrashbin(id),
+            events: await getEventsByTrashbin(id),
             trashbin: await getSingleTrashbinData(id),
             sensors: await getSensorsByTrashbin(id),
             loading: false
         });
     }
-	
+	renderSwitch(param) {
+        switch (param) {
+            case '2':
+                return 'single click';
+            case '3':
+                return 'movement start';
+            case '4':
+                return 'movement stop';
+            case '5':
+                return 'freefall';
+            case '8':
+                return 'double click';
+            case '0':
+                return 'restart';
+            case '9':
+                return 'long click';
+            case '11':
+                return 'temp max/min';
+            case '6':
+                return 'activation';
+			case '7':
+                return 'deactivation';
+            default:
+                return param;
+        }
+    }
 	timeClean(input) {
 		if(input.includes("T")) {
 			var res = input.split("T");
@@ -44,6 +73,15 @@ class TrashBinDetails extends React.Component {
 			return input;
 		}
 	}
+	
+	//this.inc = this.inc.bind(this);
+
+	inc(input) {
+		this.setState({
+			test123: input
+		});
+	}
+	
 
     render() {
         if (this.state.loading) {
@@ -52,7 +90,39 @@ class TrashBinDetails extends React.Component {
         else {
             let trashbin = this.state.trashbin;
             let events = this.state.events;
+			let pevents = this.state.pevents;
             let sensors = this.state.sensors;
+			let test = this.state.test123;
+			let viewSel;
+	
+	if(this.state.test123 === 0) {
+		viewSel = <tbody>
+                        {
+						events.map((event, index) =>
+                            <EventRow
+							event_time={this.timeClean(event.event_time)}
+                            event={this.renderSwitch(event.trigger_code)}
+                            key={index}/>
+                            ).reverse()
+						}
+                       </tbody>
+	} else {
+		viewSel = <tbody>
+                        {
+						pevents.map((event, index) =>
+                            <EventRow
+							event_time={this.timeClean(event.event_time)}
+                            event={event.event_type}
+                            key={index}/>
+                           ).reverse()
+						}
+                       </tbody>
+	}
+	
+	
+	
+	
+	console.log(test)
             return (
                 <div>
                     <nav aria-label="breadcrumb">
@@ -82,22 +152,20 @@ class TrashBinDetails extends React.Component {
                     )}
                     </tbody>
                     </table>
-                    <EventMenu />
-                    <table className="table">
+						
+					<button type="button" class="btn btn-success" onClick={() => this.inc(0)}>All Events</button>
+					<button type="button" class="btn btn-warning" onClick={() => this.inc(1)}>Bin Opened Events</button>
+					<button type="button" class="btn btn-danger" onClick={() => this.inc(2)}>Bin Emptied Events</button>
+
+					<table className="table">
                         <thead>
                             <tr>
                                 <th scope="col">Time</th>
                                 <th scope="col">Event</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            {events.map((event, index) =>
-                                <EventRow
-								event_time={this.timeClean(event.event_time)}
-                                event={event.event_type}
-                                key={index}/>
-                            ).reverse()}
-                        </tbody>
+						{viewSel}	
+                   
                     </table>
                 </div>
             )
