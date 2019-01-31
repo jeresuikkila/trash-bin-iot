4e1a962da3c77976b6377448f5a4787ded28b3d5
JussiHuot <jussi.huotari@aalto.fi>
2019-01-31 17:25:21 +0200

    xd

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+//import './MainPage.css';
+
+import TrashBinRow from './TrashBinRow';
+//import './CSS/TrashBinRow.css'
+
+// eslint-disable-next-line
+import GetTrashbinData from '../api/getTrashbinData';
+
+import { createBrowserHistory } from 'history';
+import { Redirect } from 'react-router-dom';
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { trashbins: [], binid: 0 }
+    this.handleClick = this.handleClick.bind(this);
+    this.history = createBrowserHistory();
+  }
+
+  async componentDidMount() {
+    this.setState({ trashbins: await GetTrashbinData() })
+  }
+
+  handleClick = (id) => {
+    console.log("lul: ", id);
+    this.history.push('/' + id);
+    this.setState({ binid: id })
+  }
+
+  render() {
+    if (this.state.binid !== 0) {
+      return <Redirect to={'/' + this.state.binid} />
+    }
+    let trashbins = this.state.trashbins
+    return (
+      <div className="container">
+        <h1>Trash Bin IoT</h1>
+        <h2>Trash bins</h2>
+        <table className="table table-hover" >
+          <thead>
+            <tr>
+              <th scope="col">Bin ID</th>
+              <th scope="col">Property manager</th>
+              <th scope="col">Address</th>
+              <th scope="col">Type</th>
+              <th scope="col">Latest event</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trashbins.map(trashbin =>
+              <TrashBinRow
+                key={trashbin.id}
+                clickhandler={this.handleClick}
+                bin={trashbin}
+                setBinSelected={this.props.setBinSelected} />
+            )}
+          </tbody>
+        </table>
+      </div>
+
+    )
+  }
+}
+
+export default MainPage;
---
+++ frontend/src/components/TrashBinDetails.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import './CSS/TrashBinDetails.css';
+import EventMenu from './EventMenu'
+import getPEventsByTrashbin from '../api/getPEventsByTrashbin'
+import getSingleTrashbinData from '../api/getSingleTrashbinData'
+import getSensorsByTrashbin from '../api/getSensorsByTrashbin';
+import SensorRow from './SensorRow'
+import {withRouter} from "react-router-dom";
+import trashbinimage from '../trashbinimage.jpg';
+
+
+class TrashBinDetails extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: true,
+            events: [],
+            trashbin: {},
+            sensors: []
+        }
+    }
+
+    handleClick = () => {
+        this.props.history.push("/");
+    }
+
+    async componentDidMount() {
+        var id = window.location.pathname.replace('/', '');
+        this.setState({
+            events: await getPEventsByTrashbin(id),
+            trashbin: await getSingleTrashbinData(id),
+            sensors: await getSensorsByTrashbin(id),
+            loading: false
+        });
+    }
+
+    render() {
+        if (this.state.loading) {
+            return (<p>Loading...</p>)
+        }
+        else {
+            let trashbin = this.state.trashbin;
+            let events = this.state.events;
+            let sensors = this.state.sensors;
+            return (
+                <div>
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item"><a href="/">Main Page</a></li>
+                            <li className="breadcrumb-item active" aria-current="page">{trashbin.id}</li>
+                        </ol>
+                    </nav>
+                    <h3>Trash bin details</h3>
+                    <p>Address: {trashbin.address}</p>
+                    <p>Type: {trashbin.bintype}</p>
+                    <p>Owner: {trashbin.owner}</p>
+                    <p></p>
+                    <div className="logo">
+                        <div id="trashin-pic">
+                            <img src={trashbinimage} alt="Trashbin" width="125" height="125" />
+                        </div>
+                        <div className="table sensor" >
+                            { sensors.map(sensor =>
+                                <SensorRow
+                                    key={sensor.id}
+                                    sensor={sensor}
+                                    events={events}/>
+                            )}
+                        </div>
+                    </div>
+                    <EventMenu />
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Time</th>
+                                <th scope="col">Event</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {events.map((event, index) =>
+                                <EventRow
+								event_time={timeClean(event.event_time)}
+                                event={event.event_type}
+                                key={index}/>
+                            ).reverse()}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+    }
+}
+
+const EventRow = (props) => {
+    return (
+        <tr>
+            <td>{props.event_time}</td>
+            <td> {props.event}</td>
+        </tr>
+    )
+};
+
+export const timeClean = (input) => {
+    if(input.includes("T")) {
+        var res = input.split("T");
+        var res2 = res[1].split(".");
+        var ret = res[0] + " " + res2[0];
+        return ret;
+    } else {
+        return input;
+    }
+}
+
+export default withRouter(TrashBinDetails);
