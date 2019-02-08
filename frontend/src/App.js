81764c37d3d368027b1166c3804013edfbb8ce67
vchau <van.chau@aalto.fi>
2019-02-08 13:45:37 +0200

    fix warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<button class='btn btn-light'>Hello World</button>
+				<button className='btn btn-light'>Hello World</button>
 			</div>
 		);
 	}
