d3df0b6cf7917321d844648fd31747a2f3bd5f8e
vchau <van.chau@aalto.fi>
2019-02-08 14:28:03 +0200

    add sidebar to map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import GoogleMaps from './GoogleMaps'
-
+import Sidebar from 'react-sidebar'
 const styles = {
-  width: '100%',
-  height: '100vh'
+	width: '100%',
+	height: '100vh'
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div style={styles}>
-        <GoogleMaps/>
-        </div>
-    </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			sidebarDocked: true
+		};
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<Sidebar
+					sidebar={<b>Sidebar content</b>}
+					open={this.state.sidebarOpen}
+					docked={this.state.sidebarDocked}
+					styles={{ sidebar: { background: "white", width: '400px' } }}
+				>
+				</Sidebar>
+				<div style={styles}>
+					<GoogleMaps />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
