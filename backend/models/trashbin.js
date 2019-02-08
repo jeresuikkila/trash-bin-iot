75965094d615c874251544c181440020b201a073
JussiHuot <jussi.huotari@aalto.fi>
2019-02-08 13:53:09 +0200

    db models/migrations done

--- backend/models/trashbin.js
+++ backend/models/trashbin.js
@@ -1,8 +1,31 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const trashbin = sequelize.define('trashbin', {
-    
-    
-  }, {});
-  return trashbin;
+	const trashbin = sequelize.define('trashbin', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			unique: true,
+			allowNull: false
+		},
+		bintype: {
+			type: DataTypes.STRING,
+			defaultValue: null
+		},
+		owner: {
+			type: DataTypes.STRING,
+			defaultValue: null
+		},
+		size: {
+			type: DataTypes.INTEGER,
+			defaultValue: null
+		},
+		latestEmptied: {
+			type: DataTypes.DATE,
+			defaultValue: null
+		}
+	}, {});
+	trashbin.associate = function(models) {
+		trashbin.belongsTo(models.location);
+	  };
+	return trashbin;
 };
