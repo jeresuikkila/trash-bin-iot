76a311fa01feda2c2482f6a3bdc8b6581633fb8b
vchau <van.chau@aalto.fi>
2019-02-11 22:41:40 +0200

    restructure file locations

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <button type="button" className="btn btn-light">Hello World</button>
+    </div>
+)
+
+export default App;
