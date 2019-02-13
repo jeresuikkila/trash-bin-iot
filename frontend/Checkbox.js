b0e46a94354fc6be7a06ebfdfcc4d37ff41b3b5d
vchau <van.chau@aalto.fi>
2019-02-13 10:19:10 +0200

    remove has-label-for from lint

---
+++ frontend/Checkbox.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+const Checkbox = ({ 
+type = 'checkbox', name, checked = false, onChange
+ }) => (
+    <input type={ type } name={ name } checked={ checked } onChange={ onChange } />
+);
+
+export default Checkbox;
