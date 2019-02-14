142857274f398cac020523e5d483b6cfa6879dde
Jere Suikkila <jere@suikkila.com>
2019-02-14 16:56:00 +0200

    Test response from /locations

--- backend/src/__tests__/first.test.js
+++ backend/src/__tests__/first.test.js
@@ -12,8 +12,12 @@ describe('GET / ', () => {
 })
 
 describe('GET /location', () => {
-  test('It should respond', async () => {
+  test('It should return two locations in JSON format', async () => {
     const response = await request(app).get('/location')
     expect(response.statusCode).toBe(200)
+    // console.log(response.body)
+    expect(response.type).toBe('application/json')
+    const locations = JSON.parse(response.text)
+    expect(locations.length).toEqual(2)
   })
 })
