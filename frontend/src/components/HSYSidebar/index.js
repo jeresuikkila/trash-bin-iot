5da97d47fd51f61130937a54430ee4ce6445809e
unknown <mimsi.seppala@gmail.com>
2019-02-20 00:35:24 +0200

    add location status

--- frontend/src/components/HSYSidebar/index.js
+++ frontend/src/components/HSYSidebar/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import Filter from '../Filter';
-import './styles.css'
+import './styles.css';
+import hsylogo from '../../static/hsy-logo.png';
+
 
 class HSYSidebar extends Component {
   render() {
     const { onFilterChange, filters } = this.props;
     return (
         <div id="sidebar">
-            <p className="title">HSY SMART WASTE</p>
+            <p className="sidebar-title">HSY SMART WASTE</p>
             <div className="white-background">
                 <Filter
                   onFilterChange={ onFilterChange }
                   filters={ filters }
                 />
             </div>
+            <div className="light-teal-rectangle"></div>
+            <div className="dark-teal-rectangle"></div>
+            <p className="location-status-title">LOCATION STATUS</p>
+            <img src={ hsylogo } className="hsy-logo"></img>
         </div>
     );
   }
