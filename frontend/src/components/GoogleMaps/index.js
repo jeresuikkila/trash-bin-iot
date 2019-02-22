4446c89d2cb2074c98c12f187b9ca90f5d49ca8f
Henri <henri.kaijomaa@aalto.fi>
2019-02-22 14:45:07 +0200

    made version of that works with .env

--- frontend/src/components/GoogleMaps/index.js
+++ frontend/src/components/GoogleMaps/index.js
@@ -5,10 +5,10 @@ import {
 } from 'react-google-maps';
 
 const trashbin = require('../../static/trashbin.png')
-
+require('dotenv').config()
 const GoogleMaps = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=', // add google maps api key to the end of the line.
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key='+ process.env.REACT_APP_GOOGLE_API_KEY, // add google maps api key to the end of the line.
     loadingElement: <div style={ { height: '100%' } } />,
     containerElement: <div style={ { height: '100%' } } />,
     mapElement: <div style={ { height: '100%' } } />,
