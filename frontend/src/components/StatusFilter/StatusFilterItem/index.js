7193a61f1997d02c832faf907b3a88014bbc62bc
unknown <mimsi.seppala@gmail.com>
2019-02-24 13:16:20 +0200

    fixed checkboxes

--- frontend/src/components/StatusFilter/StatusFilterItem/index.js
+++ frontend/src/components/StatusFilter/StatusFilterItem/index.js
@@ -3,14 +3,19 @@ import './styles.css';
 
 const StatusFilterItem = ({ item, handleChange, checked = false }) => (
     <div>
-        <input
-          id={ item.name }
-          name={ item.name }
-          type="checkbox"
-          onChange={ handleChange }
-          checked={ checked }
-        />
-        <label className="status-label" htmlFor={ item.name } />
+        <label className="status-filter-item" htmlFor={ item.name }> 
+            <input
+            id={ item.name }
+            name={ item.name }
+            type="checkbox"
+            onChange={ handleChange }
+            checked={ checked }
+            />
+            <label htmlFor = {item.name}>
+                <input type="hidden"></input>
+            </label>
+            <span className="status-filter-item-name" > {`${ item.name}`} </span>
+        </label>
     </div>
 )
 
