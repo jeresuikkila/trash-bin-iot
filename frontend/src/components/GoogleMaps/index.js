1b5f0ef40a23383157c1cb3c738deb30d66e1808
Henri <henri.kaijomaa@aalto.fi>
2019-02-27 18:32:14 +0200

    removed more dead code

--- frontend/src/components/GoogleMaps/index.js
+++ frontend/src/components/GoogleMaps/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { compose, withProps, } from 'recompose';
+import { compose, withProps } from 'recompose';
 import {
   withScriptjs, withGoogleMap, GoogleMap, Marker,
 } from 'react-google-maps';
