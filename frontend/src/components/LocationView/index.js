ff62c43116315fd0046e5a6e258e8ee697f9c36a
Veera Alt <veera.alt@aalto.fi>
2019-03-05 23:25:59 +0200

    Add address and back button css to LocationView

--- frontend/src/components/LocationView/index.js
+++ frontend/src/components/LocationView/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import hsylogo from '../../static/hsy-logo.png';
+import backarrow from '../../static/back-arrow.png';
 import './styles.css'
 
 const LocationView = (props) => {
   const { toggleLocationView, location } = props;
   return (
       <div id="sidebar">
-          <button type="button" className="back-button-round" onClick={ () => toggleLocationView(location.id) } />
+          <button type="button" className="back-button-round" onClick={ () => toggleLocationView(location.id) }>
+          <img src={ backarrow } className="back-arrow" alt="arrow" />
           <p className="back-text">Back</p>
+          </button>
           <div className="location-white-background">
-              <p className="">{location.address.split(',')[ 0 ]}</p>
-              <p className="">{location.customer}</p>
+              <p className="address">{location.address.split(',')[ 0 ]}</p>
+              <p className="owner-text">{location.customer}</p>
           </div>
           <img src={ hsylogo } className="hsy-logo" alt="logo" />
       </div>
