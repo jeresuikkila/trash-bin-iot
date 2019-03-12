b1032099507d9181b65c84e341c768783f9a5309
unknown <mimsi.seppala@gmail.com>
2019-03-12 11:57:03 +0200

    added scaledSize

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -175,3 +175,19 @@ export function getMarkerUrl(location, locationOk, locationOverflow, locationOve
   }
   else return locationOk;
 }
+
+export function getScaledSize(location) {
+
+  const overflowLocations = getOverflowLocations(new Array(location));
+  const overdueLocations = getOverdueLocations(new Array(location));
+
+  if(overflowLocations.includes(location) && overdueLocations.includes(location)){
+    return new window.google.maps.Size(21, 21);
+  }
+  else if(overflowLocations.includes(location) && !(overdueLocations.includes(location))){
+    return new window.google.maps.Size(15, 15);
+  }
+  else return new window.google.maps.Size(15, 15);
+}
+
+
