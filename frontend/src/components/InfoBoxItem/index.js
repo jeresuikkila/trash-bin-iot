82fec575166ccef1ba4fa1a1f3b5dafc8e85a358
vchau <van.chau@aalto.fi>
2019-03-12 22:12:12 +0200

    added new icons

--- frontend/src/components/InfoBoxItem/index.js
+++ frontend/src/components/InfoBoxItem/index.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import './styles.css'
-import BioWasteIcon from '../../static/biowaste.png';
-import CardBoardIcon from '../../static/carton.png';
-import MetalIcon from '../../static/metal.png';
-import GeneralIcon from '../../static/general.png';
-import GlassIcon from '../../static/glass.png';
-import PlasticIcon from '../../static/plastic.png';
+import biowasteOK from '../../static/biowaste-ok.png';
+import cardboardOK from '../../static/cardboard-ok.png';
+import metalOK from '../../static/metal-ok.png';
+import generalOK from '../../static/general-ok.png';
+import glassOK from '../../static/glass-ok.png';
+import plasticOK from '../../static/plastic-ok.png';
+import biowasteFull from '../../static/biowaste-full.png';
+import cardboardFull from '../../static/cardboard-full.png';
+import metalFull from '../../static/metal-full.png';
+import generalFull from '../../static/general-full.png';
+import glassFull from '../../static/glass-full.png';
+import plasticFull from '../../static/plastic-full.png';
 import alert from '../../static/alert.png';
 
-const imageMap = {
-  Biowaste: BioWasteIcon,
-  Cardboard: CardBoardIcon,
-  Metal: MetalIcon,
-  General: GeneralIcon,
-  Glass: GlassIcon,
-  Plastic: PlasticIcon,
+const iconsOK = {
+  Biowaste: biowasteOK,
+  Cardboard: cardboardOK,
+  Metal: metalOK,
+  General: generalOK,
+  Glass: glassOK,
+  Plastic: plasticOK,
 }
 
-const getIconBackground = (type, fillStatus) => ((fillStatus === 100) ? (`${ type.toLowerCase() }-full`) : 'bin-ok')
+const iconsFull = {
+  Biowaste: biowasteFull,
+  Cardboard: cardboardFull,
+  Metal: metalFull,
+  General: generalFull,
+  Glass: glassFull,
+  Plastic: plasticFull,
+}
 
 const InfoBoxItem = ({ type, fillStatus, overflowStatus }) => (
     <div className="infoboxitem">
-        <img src={ imageMap[ type ] } className={ getIconBackground(type, fillStatus) } alt="test" />
+        {fillStatus === 100 ? (
+            <img src={ iconsFull[ type ] } className="infobox-item" alt="test" />
+        ) : (
+            <img src={ iconsOK[ type ] } className="infobox-item" alt="test" />
+        )}
         {overflowStatus ? (
             <img src={ alert } className="alerticon" alt="alert" />
-        ) : null}
+        ) : (
+          null
+        )}
     </div>
 )
 export default InfoBoxItem;
