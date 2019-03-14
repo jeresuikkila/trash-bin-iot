16339dd22b512b2a9fdb2c26b335738aa9c3f3bb
unknown <mimsi.seppala@gmail.com>
2019-03-15 00:44:58 +0200

    buggy accordion

--- frontend/src/components/Accordion/index.js
+++ frontend/src/components/Accordion/index.js
@@ -2,52 +2,33 @@
 import React from 'react';
 
 
-const Accordion = (props) => (
-    <div className="accordion" id="accordionExample">
-    <div className="card">
-        <div className="card-header" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        <h5 className="mb-0">
-            <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-            Collapsible Group Item #1
-            </button>
-        </h5>
-        </div>
+const Accordion = ({location}) => {
+    
+    let wasteTypes = location.trashbins.map( bin => bin.wasteType);
+    wasteTypes = [...new Set(wasteTypes)];
 
-        <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-        <div className="card-body">
-            Anim pariatur cliche reprehenderit
-        </div>
-        </div>
+    let trashbinsByType = new Map();
+
+    wasteTypes.forEach(type => {
+        const trashbins = location.trashbins.filter(bin => bin.wasteType === type)
+        console.log(trashbins)
+        trashbinsByType.set(type, trashbins)        
+    });
+
+    return (
+    <div className="accordion" id="accordionExample">
+        {
+            wasteTypes.map( type =>(
+                <div className="card">
+                    <AccordionTitle 
+                        type={type}
+                        trashbins={trashbinsByType.get({type})}
+                    />
+                </div>
+            ))
+        }
     </div>
-    <div className="card">
-        <div className="card-header" id="headingTwo">
-        <h5 className="mb-0">
-            <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
-            </button>
-        </h5>
-        </div>
-        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-        <div className="card-body">
-            enim eiusmod high life accusamus terry richardson 
-        </div>
-        </div>
-    </div>
-    <div className="card">
-        <div className="card-header" id="headingThree">
-        <h5 className="mb-0">
-            <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Collapsible Group Item #3
-            </button>
-        </h5>
-        </div>
-        <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-        <div className="card-body">
-            ad squid. 3 wolf moon officia aute
-        </div>
-        </div>
-    </div>
-    </div>
-)
+    )
+}
 
 export default Accordion;
