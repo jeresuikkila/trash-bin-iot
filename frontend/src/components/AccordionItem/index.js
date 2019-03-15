550117ddec0ecc181a4b3212fb36194279a31191
unknown <mimsi.seppala@gmail.com>
2019-03-15 12:45:39 +0200

    css

--- frontend/src/components/AccordionItem/index.js
+++ frontend/src/components/AccordionItem/index.js
@@ -6,7 +6,7 @@ const AccordionItem = ({trashbins, type, locationId}) => {
     
     return(
         <div className="card">
-            <div className="card-header" id={type+locationId+'-heading'} data-toggle="collapse" data-target={'#'+type+locationId} aria-expanded="false" aria-controls={type+locationId}>
+            <div className={"card-header-"+type.toLowerCase()} id={type+locationId+'-heading'} data-toggle="collapse" data-target={'#'+type+locationId} aria-expanded="false" aria-controls={type+locationId}>
                 <p>{type}</p>
             </div>
             <div id={type+locationId} className="collapse" aria-labelledby={type+locationId+'-heading'} data-parent={"#accordion"+locationId}>
