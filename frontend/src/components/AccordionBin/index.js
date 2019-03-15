8fe904482bb2cbbafa5be0d3a994d78c0ca84755
unknown <mimsi.seppala@gmail.com>
2019-03-15 11:13:40 +0200

    add AccordionBin

---
+++ frontend/src/components/AccordionBin/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AccordionBin = () => {
+
+    return(
+        <div>
+             
+        </div>
+    )
+}
+
+
+export default AccordionBin;
--- frontend/src/components/AccordionItem/index.js
+++ frontend/src/components/AccordionItem/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AccordionBin from '../AccordionBin';
 
 const AccordionItem = ({trashbins, type, locationId}) => {
     
