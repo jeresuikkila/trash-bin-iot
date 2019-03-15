6890e91f1e18cedcad9dcacc24e44e0f8dc2599c
vchau <van.chau@aalto.fi>
2019-03-15 14:18:35 +0200

    css adjust arrow

--- frontend/src/components/AccordionItem/index.js
+++ frontend/src/components/AccordionItem/index.js
@@ -7,6 +7,7 @@ import metal from '../../static/metal.png';
 import general from '../../static/general.png';
 import glass from '../../static/glass.png';
 import plastic from '../../static/plastic.png';
+import accordionArrow from '../../static/accordion-arrow.png'
 
 const icons = {
   Biowaste: biowaste,
@@ -21,7 +22,11 @@ const AccordionItem = ({ trashbins, type, locationId }) => (
     <div className="card">
         <div className={ `card-header-${ type.toLowerCase() }` } id={ `${ type + locationId }-heading` } data-toggle="collapse" data-target={ `#${ type }${ locationId }` } aria-expanded="false" aria-controls={ type + locationId }>
             <img src={ icons[ type ] } className="type-icon" alt="type" />
-            <p className="header-title">{type.toUpperCase()}</p>
+            <p className="header-title">
+                {type.toUpperCase()}
+                <img src={ accordionArrow } className="arrow" alt="arrow" />
+            </p>
+            
         </div>
         <div id={ type + locationId } className="collapse" aria-labelledby={ `${ type + locationId }-heading` } data-parent={ `#accordion${ locationId }` }>
             <div className="card-body">
