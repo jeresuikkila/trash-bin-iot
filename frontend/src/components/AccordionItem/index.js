08f98b111c3daecbd9c434ed3e01c3a959cc33be
vchau <van.chau@aalto.fi>
2019-03-23 21:26:15 +0200

    refactor

--- frontend/src/components/AccordionItem/index.js
+++ frontend/src/components/AccordionItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AccordionBin from '../AccordionBin';
+import TrashbinIcon from '../TrashBinIcon';
 import './styles.css'
 import biowaste from '../../static/biowaste.png';
 import cardboard from '../../static/carton.png';
@@ -8,8 +9,6 @@ import general from '../../static/general.png';
 import glass from '../../static/glass.png';
 import plastic from '../../static/plastic.png';
 import accordionArrow from '../../static/accordion-arrow.png'
-import trashbinOk from '../../static/trashbin-white.png';
-import trashbinFull from '../../static/trashbin-orange.png';
 import alert from '../../static/alert.png';
 
 const icons = {
@@ -23,29 +22,25 @@ const icons = {
 
 const AccordionItem = ({
   trashbins, type, locationId, overflowTypes,
-}) => {
-  const fullTrashbins = trashbins.filter(bin => bin.fillStatus === 100).length
-  const okTrashbins = trashbins.length - fullTrashbins;
-
-  return (
-      <div className="card">
-          <div className={ `card-header-${ type.toLowerCase() }` } id={ `${ type + locationId }-heading` } data-toggle="collapse" data-target={ `#${ type }${ locationId }` } aria-expanded="false" aria-controls={ type + locationId }>
-              <img src={ icons[ type ] } className="type-icon" alt="type" />
-              <p className="header-title">{type.toUpperCase()}</p>
-              <img src={ accordionArrow } className="arrow" alt="arrow" />
-              <div className="number-container">
-                  <img src={ trashbinFull } className="trashbin-icon bin-full" alt="full-trash-icon" />
-                  <div className="number-text number-full">{fullTrashbins}</div>
-              </div>
-              <div className="number-container">
-                  <img src={ trashbinOk } className="trashbin-icon bin-ok" alt="ok-trash-icon" />
-                  <div className="number-text number-ok">{okTrashbins}</div>
-              </div>
-              {overflowTypes.includes(type) && <img src={ alert } className="alert" alt="alert" />}
-          </div>
-          <div id={ type + locationId } className="collapse" aria-labelledby={ `${ type + locationId }-heading` } data-parent={ `#accordion${ locationId }` }>
-              <div className="card-body">
-                  {
+}) => (
+    <div className="card">
+        <div className={ `card-header ${ type.toLowerCase() }` } id={ `${ type + locationId }-heading` } data-toggle="collapse" data-target={ `#${ type }${ locationId }` } aria-expanded="false" aria-controls={ type + locationId }>
+            <img src={ icons[ type ] } className="type-icon" alt="type" />
+            <p className="header-title">{type.toUpperCase()}</p>
+            <img src={ accordionArrow } className="arrow" alt="arrow" />
+            <TrashbinIcon
+              trashbins={ trashbins }
+              status="full"
+            />
+            <TrashbinIcon
+              trashbins={ trashbins }
+              status="ok"
+            />
+            {overflowTypes.includes(type) && <img src={ alert } className="alert" alt="alert" />}
+        </div>
+        <div id={ type + locationId } className="collapse" aria-labelledby={ `${ type + locationId }-heading` } data-parent={ `#accordion${ locationId }` }>
+            <div className="card-body">
+                {
                         trashbins.map( (bin, i) => (
                             <AccordionBin
                               trashbin={ bin }
@@ -56,10 +51,9 @@ const AccordionItem = ({
                             />
                         ))
                     }
-              </div>
-          </div>
-      </div>
-  )
-}
+            </div>
+        </div>
+    </div>
+)
 
 export default AccordionItem;
